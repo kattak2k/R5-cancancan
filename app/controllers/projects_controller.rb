54415da5c0e072da8f76330d4f5ecd0be3fd1cac
kattak2k <kkiran.ror@gmail.com>
2017-03-27 01:57:50 +0530

    remove duplication in project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,55 +1,59 @@
 class ProjectsController < ApplicationController
-  def index
-    @projects = Project.all
-    authorize! :index, @project
-  end
+  
+  before_action :load_projects, only: :index
+  
+  load_and_authorize_resource
 
-  def new
-    @project = Project.new
-    authorize! :new, @project
-  end
+  # def index
+  #   @projects = Project.all
+  #   authorize! :index, @project
+  # end
 
-  def create
-    @project = Project.new(project_params)
+  # def new
+  #   @project = Project.new
+  #   authorize! :new, @project
+  # end
+
+ def create
     if @project.save
       flash[:success] = 'Project was saved!'
       redirect_to root_path
     else
       render 'new'
     end
-    authorize! :create, @project
   end
 
-  def edit
-  @project = Project.find(params[:id])
-  authorize! :edit, @project
-end
+#   def edit
+#   @project = Project.find(params[:id])
+#   authorize! :edit, @project
+# end
+
 
 def update
-  @project = Project.find(params[:id])
-  if @project.update_attributes(project_params)
-    flash[:success] = 'Project was updated!'
-    redirect_to root_path
-  else
-    render 'edit'
+    if @project.update_attributes(project_params)
+      flash[:success] = 'Project was updated!'
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
-  authorize! :update, @project
-end
 
-def destroy
-  @project = Project.find(params[:id])
-  if @project.destroy
-    flash[:success] = 'Project was destroyed!'
-  else
-    flash[:warning] = 'Cannot destroy this project...'
+  def destroy
+    if @project.destroy
+      flash[:success] = 'Project was destroyed!'
+    else
+      flash[:warning] = 'Cannot destroy this project...'
+    end
+    redirect_to root_path
   end
-  redirect_to root_path
-  authorize! :destroy, @project
-end
 
   private
 
   def project_params
     params.require(:project).permit(:title)
   end
+
+  def load_projects
+  @projects = Project.accessible_by(current_ability).order('created_at DESC')
+end
 end
